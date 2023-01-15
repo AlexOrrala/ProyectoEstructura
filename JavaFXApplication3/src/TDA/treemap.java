2e9a3c72263e4de4d84ef7df1d6b95e666fcce1d
Alexorrala <ajorrala@espol.edu.ec>
2023-01-15 12:37:22 -0500

    package

---
+++ JavaFXApplication3/src/TDA/treemap.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TDA;
+
+/**
+ *
+ * @author alex_
+ */
+public class treemap {
+    private String directory;
+    
+    
+    
+    public void GetDirectory(String directory){
+        this.directory = directory;
+    }
+    
+    public void cargarubicacion(){
+        
+    
+    }
+    
+    
+    
+    
+    
+}
